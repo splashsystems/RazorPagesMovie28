2bebeb62fd802c6014eb8f519b29b8c011353082
Scot <bscot.hagen@gmail.com>
2021-08-12 11:16:28 -0700

    Added search by partial title

---
+++ RazorPagesMovie28/Data/RazorPagesMovie28Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie28.Models;
+
+namespace RazorPagesMovie28.Data
+{
+    public class RazorPagesMovie28Context : DbContext
+    {
+        public RazorPagesMovie28Context (DbContextOptions<RazorPagesMovie28Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie28.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie28/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie28.Data;
+using RazorPagesMovie28.Models;
+
+namespace RazorPagesMovie28.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie28.Data.RazorPagesMovie28Context _context;
+
+        public IndexModel(RazorPagesMovie28.Data.RazorPagesMovie28Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
